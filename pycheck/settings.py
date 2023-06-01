854d26a90b0a908a83cae544c9fb94ef6da1c72d
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2023-06-01 16:39:38 +0530

    update the database

--- pycheck/settings.py
+++ pycheck/settings.py
@@ -12,7 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
+# for security
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,11 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-39i+p5r$2^(@_6g4opjy+y5b$k%dtt^-7q7+!l%lts4nuhop-3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
-# ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+# ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -77,23 +81,30 @@ WSGI_APPLICATION = 'pycheck.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+import dj_database_url
+
+DATABASES = {
+
+    'default': dj_database_url.parse(env('DATABASE_URL'))
+}
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'GMr7VBsLBX7UhAKw3tLq',
-#         'HOST': 'containers-us-west-151.railway.app',
-#         'PORT': '7990',
+#         'NAME': 'blog_i3tx',
+#         'USER': 'gaurav1141',
+#         'PASSWORD': '9kMdfZwZkYrWI408UmUVstGAejWxtFlv',
+#         'HOST': 'dpg-chs6vmm4dadfn610nnbg-a',
+#         'PORT': '5432',
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
